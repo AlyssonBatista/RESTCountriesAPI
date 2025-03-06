2e6f266c6256983dd705cdfcf6d75acb2a138813
Alysson Batista de Souza <alyssonbatista241@gmail.com>
2025-03-06 00:10:08 -0300

    add libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,70 @@
-import { useState } from 'react'
-import './App.css'
-import { Button, Stack } from '@mui/material'
+import { useState } from "react";
+import "./App.css";
+import { Button, Card, CardContent, Grid2, TextField } from "@mui/material";
+import { css } from "@emotion/css";
 
 function App() {
-  const [count, setCount] = useState(0)
-function somar(a:number,b:number){
-  return a+b
-}
-  console.log(somar(34,23))
+  const [total, setTotal] = useState(0);
+  const [palavra, setpalavra] = useState("");
+  function somar() {
+    return setTotal(total + 1);
+  }
+  function normallizarTexto(texto: string) {
+    return texto.trim().toLowerCase();
+  }
+
+  const cardCont = css`
+    card: {
+      margin-bottom: 1rem ;
+    }
+  `;
 
   return (
-    <>
-      <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      </Stack>
-    </>
-  )
+    <Grid2>
+      <Card  style={{marginBottom: '1rem'}}>
+        <CardContent>
+          <h1>{total}</h1>
+          <TextField
+            color="primary"
+            size="small"
+            id="filled-basic"
+            label="Filled"
+            variant="standard"
+          />
+          <Button
+            color="primary"
+            size="large"
+            variant="contained"
+            onClick={somar}
+          >
+            Somar
+          </Button>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardContent>
+          <h1>Bônus de 100 - 20% do total:</h1>
+          <label>Total</label>
+          <TextField
+            type="number"
+            color="primary"
+            size="small"
+            id="filled-basic"
+            label="Filled"
+            variant="standard"
+          />
+          <Button
+            color="primary"
+            size="large"
+            variant="contained"
+            onClick={somar}
+          >
+            Somar
+          </Button>
+        </CardContent>
+      </Card>
+    </Grid2>
+  );
 }
 
-export default App
+export default App;
